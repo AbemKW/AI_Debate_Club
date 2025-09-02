e702e16a549c24e1d86fedf39790947394c674e7
Abem Woldesenbet <abemkibatu101@gmail.com>
2025-09-01 21:37:45 -0500

    Modularized the whole project

---
+++ src/debate_state.py
@@ -0,0 +1,12 @@
+from typing import Annotated, TypedDict
+from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage
+
+class DebateState(TypedDict):
+    topic: str
+    pro_argument: str
+    con_argument: str
+    current_speaker: str
+    chat_history: Annotated[list[BaseMessage], add_messages]
+    round: int
+    max_rounds: int
---
+++ src/main.py
@@ -0,0 +1,49 @@
+from langgraph.graph import StateGraph, END
+from agents.pro_agent import pro_node
+from agents.con_agent import con_node
+from agents.moderator_agent import moderator_node
+from debate_state import DebateState
+
+
+graph = StateGraph(DebateState)
+graph.add_node("pro", pro_node)
+graph.add_node("con", con_node)
+graph.add_node("moderator", moderator_node)
+
+graph.set_entry_point("pro")
+
+def route_speaker(state):
+    if(state["round"] > state["max_rounds"]):
+        return "moderator"
+    if(state["current_speaker"] == "pro"):
+        return "pro"
+    elif state["current_speaker"] == "con":
+        return "con"
+    else:
+        return "moderator"
+
+# Use conditional edge from a "router" step
+graph.add_conditional_edges(
+    source="pro",  # After pro speaks
+    path=route_speaker
+)
+graph.add_conditional_edges(
+    source="con",  # After con speaks
+    path=route_speaker
+)
+graph.add_conditional_edges(
+    source="moderator",
+    path=lambda x: END  # Moderator ends the flow
+)
+
+app = graph.compile()
+
+result = app.invoke({
+    "topic": "The impact of AI on society",
+    "chat_history": [],
+    "pro_argument": "",
+    "con_argument": "",
+    "current_speaker": "pro",
+    "round": 0,
+    "max_rounds": 3
+})
